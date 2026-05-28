4db26e92690b5d7460b34996f93b3c17405f1f33
Caio César Miranda de Freitas <wrhjk2000@gmail.com>
2026-05-28 18:11:29 -0300

    Refactor output formatting and add newlines

--- CustoBeneficio-Mercado.py
+++ CustoBeneficio-Mercado.py
@@ -1,5 +1,4 @@
-print('Compare os produtos e descubra qual vale mais a pena')
-print()
+print('\n\nCompare os produtos e descubra qual vale mais a pena\n')
 
 peso1 = int(input('Digite o peso do 1º Produto: '))
 valor1 = float(input('Agora digite o valor do 1º Produto: '))
@@ -7,12 +6,9 @@ peso2 = int(
     input('Certo, agora vamos comparar com o 2º Produto, digite o peso: '))
 valor2 = float(input('Pra finalizar o valor deste 2º Produto: '))
 
-pkilo1 = float(1000*(valor1/peso1))  # Preço do kg do 1º Produto
-pkilo2 = float(1000*(valor2/peso2))  # Preço do kg do 2º Produto
-print()
-print()
+pkilo1 = 1000*(valor1/peso1) # Preço do kg do 1º Produto
+pkilo2 = 1000*(valor2/peso2) # Preço do kg do 2º Produto
 
-print(f'O 1º Produto está custando {pkilo1} reais o kg')
-print()
+print(f'\n\nO 1º Produto está custando {pkilo1:.2f} reais o kg')
 
-print(f'Enquanto o 2º Produto está {pkilo2} reais o kg')
+print(f'\nEnquanto o 2º Produto está {pkilo2:.2f} reais o kg')
